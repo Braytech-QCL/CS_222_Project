5a6d21d6faf88122914ed63a37f43d5d0baa0af9
Connor <connor@mcgillnet.net>
2022-11-10 18:40:05 -0500

    made classes and declared vars, added description for var

--- BoardDesign.h
+++ BoardDesign.h
@@ -1,5 +1,18 @@
+/*
+Catalogues the physical characteristics of a GPU board
+
+
+*/
 #pragma once
 class BoardDesign
 {
+public:
+
+private:
+	double slotWidth //how many slots a gpu card takes up
+	int TDP //Thermal Design Power, maximum power that a subsystem is allowed to draw for a ‘real world’ application
+	string videoOutputs //the output ports on the GPU that can be connected to monitors, usally hdmi, display port, and vga/dvi
+	string powerConnector //the power conenctor used to plug in the gpu. lower end gpus might not need one if tdp if under 75
+	double length, width, height //the actual size measurements of the GPU
 };
 
--- GPU.h
+++ GPU.h
@@ -1,5 +1,18 @@
+/*
+Main class that stores GPU information, used 3 parent classes to store specific information
+
+*/
 #pragma once
+#include "BoardDesign.h"
+#include "Processor.h"
+#include "GenArchEnum.h"
+
 class GPU
 {
+public:
+
+private:
+	string releaseDate //the date the GPU was released for purchase
+	int MSRP //the manufacturers suggested retail price
 };
 
